b17bb0078105d369a2e2fa77b3e511a4babbf781
verygoodcreativeco <nick@verygoodcreative.co>
2026-03-17 21:24:10 -0700

    feat: configure Tailwind with Dieselmatic brand system

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          orange: '#f27226',
+          'orange-dark': '#e66425',
+          black: '#1e1e1e',
+          'black-alt': '#292828',
+          white: '#fafcff',
+          blue: '#00aed9',
+          red: '#cf1117',
+          purple: '#6845ff',
+          green: '#10a38d',
+        }
+      },
+      fontFamily: {
+        headline: ['"Franklin Gothic"', 'Impact', 'sans-serif'],
+        body: ['"Public Sans"', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
